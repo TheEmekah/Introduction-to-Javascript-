8cc7f96f2fb85a8c43c4927dd36b3051b0b79808
TheEmekah <51264153+TheEmekah@users.noreply.github.com>
2022-06-25 16:42:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Uzoh Daniel Chukwuemeka')
+console.log('Height: 6ft 2 in');
+console.log('Country: Nigeria');
